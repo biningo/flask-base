3f462894b6e74bb847a90d6774f62b3537482fa8
biningo <biningo@yeah.net>
2020-07-27 10:54:43 +0800

    test

--- hello/hello.py
+++ hello/hello.py
@@ -3,13 +3,13 @@ app = Flask(__name__)
 
 @app.route("/ping")
 def ping():
-    return "Pong"
+    return "Pong Pong"
 
-@app.route("/")
+@app.route("/hello")
 def hello():
     return "hello,world"
 
 
 # https://segmentfault.com/a/1190000022139239
 if __name__ == '__main__':
-    app.run(port="8080",debug=True) #设置debug可以动态刷新更改
+    app.run(port="8080") #设置debug可以动态刷新更改
