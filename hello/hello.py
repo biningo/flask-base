e83ac7da2f07d417ec394935584c4f22a945d8c6
biningo <biningo@yeah.net>
2020-07-29 14:24:42 +0800

    hello

--- hello/hello.py
+++ hello/hello.py
@@ -13,3 +13,6 @@ def hello():
 # https://segmentfault.com/a/1190000022139239
 if __name__ == '__main__':
     app.run(port="8080") #设置debug可以动态刷新更改
+
+import logging
+from logging import StreamHandler
